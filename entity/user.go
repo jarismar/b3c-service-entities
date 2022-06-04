22c85a2777d631273ddf9f17c530725e56510324
jarismar <jarismar@gmail.com>
2022-06-04 07:39:23 -0300

    add user external uuid field.

--- entity/user.go
+++ entity/user.go
@@ -1,7 +1,10 @@
 package entity
 
+import "github.com/google/uuid"
+
 type User struct {
-	UserID   int
-	UserUUID string
-	UserName string
+	UserID           int
+	UserUUID         uuid.UUID
+	UserExternalUUID string
+	UserName         string
 }
