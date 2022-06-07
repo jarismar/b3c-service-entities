7f14fb2679ea4c0df7240df9490a5590a93cbbc6
jarismar <jarismar@gmail.com>
2022-06-07 08:17:00 -0300

    add remaining entities

---
+++ entity/invoice.go
@@ -0,0 +1,31 @@
+package entity
+
+import "time"
+
+type InvoiceTax struct {
+	Id    int64
+	Tax   *Tax
+	Value float64
+}
+
+type InvoiceItem struct {
+	Id      int64
+	Company *Company
+	Qty     int64
+	Balance int64
+	Price   float64
+	Debit   bool
+}
+
+type Invoice struct {
+	Id          int64
+	Number      int64
+	User        *User
+	FileName    string
+	MarketDate  time.Time
+	BillingDate time.Time
+	RawValue    float64
+	NetValue    float64
+	Items       []InvoiceItem
+	Taxes       []InvoiceTax
+}
---
+++ entity/tax.go
@@ -0,0 +1,7 @@
+package entity
+
+type Tax struct {
+	Id     int64
+	Code   string
+	Source string
+}
